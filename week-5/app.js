6fa24e38ddcfdd165d4f62c57e21e9d8eb3bc681
Bladimir Arroyo <bladimir.ab@gmail.com>
2024-03-15 09:35:08 -0600

    Including writing in html with JS

--- week-5/app.js
+++ week-5/app.js
@@ -1,3 +1,4 @@
+
 function saveForm() {
   const formElements = document.querySelectorAll('input.form-control, select, input.form-check-input[checked]')
 
@@ -18,3 +19,26 @@ function restoreFormData(){
   }
 }
 
+function showUserDataTable(){
+  const data = localStorage.getItem('tempData');
+  if(data){
+    const formData = JSON.parse(data);
+    // const row = document.createElement('tr');
+    // const nameCol = document.createElement('td');
+    // nameCol.innerHTML = 'Bladimir';
+    // row.appendChild(nameCol);
+    // const userNameCol = document.createElement('td');
+    // userNameCol.innerHTML = 'barroyo';
+    // row.appendChild(userNameCol);
+    // const provinceCol = document.createElement('td');
+    // provinceCol.innerHTML = 'Alajuela';
+    // row.appendChild(provinceCol);
+    // document.getElementById('userDataTable').appendChild(row);
+    let rowHtml = '<tr>';
+    Object.keys(formData).forEach(elementKey => {
+      rowHtml += `<td>${formData[elementKey]}</td>`;
+    });
+    rowHtml += '</tr>';
+    document.getElementById('userDataTable').innerHTML = rowHtml;
+  }
+}
